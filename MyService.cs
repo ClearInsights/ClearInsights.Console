e7507ee0cd8081b637a7d21b3288e7e4d4a46efb
Jason Archer <106941137+jasoncarcher@users.noreply.github.com>
2022-08-26 18:26:36 -0400

    Initial ClearInsights.Console upload

---
+++ MyService.cs
@@ -0,0 +1,44 @@
+﻿using ClearInsights.Monitoring.Metric;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ClearInsights.Console
+{
+    public interface IMyService
+    {
+        Task ProcessComplete();
+        Task ProcessCompleteWithError();
+    }
+    public class MyService : IMyService
+    {
+        protected ILogger Logger;
+
+        public MyService(ILogger<MyService> logger) => Logger = logger;
+
+        public async Task ProcessComplete()
+        {
+            var metric = new DynamicMetric("e645fe78-e2b3-11ec-8f8e-000d3a4384cb", "bQ7Qh/fWdbXthRNuHoDPzOJJPCn5vSFTEjk7yHlGgLDT7eLcFK2h+2h3W0LCMjgcBTjqC9Ns+NlTrmYqi58eVA==");
+            var person = new Person()
+            {
+                Name = "",
+                Address = ""
+            };
+            metric.SetPayload(JsonConvert.SerializeObject(person));
+            await metric.SendAsync();
+            Logger.LogInformation("MyService Process Complete");
+        }
+
+        public async Task ProcessCompleteWithError()
+        {
+            var metric = new DynamicMetric("e645fe78-e2b3-11ec-8f8e-000d3a4384cb", "bQ7Qh/fWdbXthRNuHoDPzOJJPCn5vSFTEjk7yHlGgLDT7eLcFK2h+2h3W0LCMjgcBTjqC9Ns+NlTrmYqi58eVA==");
+            await metric.SendAsync();
+            throw new Exception("Sample Exception");
+        }
+    }
+
+    public class Person
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using ClearInsights.Console;
+using ClearInsights.Logging;
+using ClearInsights.Monitoring;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(builder =>
+        builder.ClearProviders()
+            //Add to capture logs with ClearInsights Logging product
+            .AddClearInsightsLogger(configuration =>
+            {
+                configuration.ApiKey = "{ApiKey}";
+                configuration.Secret = "{Environment Client Secret}";
+                configuration.ApplicationName = "{Application Name}";
+            }))
+    .ConfigureServices(services =>
+    {
+        // Add to capture performance metrics with the Monitoring product
+        services.AddClearInsightsMonitor(configuration =>
+        {
+            configuration.ApiKey = "{ApiKey}";
+            configuration.Secret = "{Environment Client Secret}";
+            configuration.ApplicationName = "{Application Name}";
+        });
+        services.AddTransient<IMyService, MyService>();
+    })
+            .Build();
+await host.StartAsync();
+
+//Add to use ClearInsights global error handling.
+//This will automatically catch any unhandled exceptions
+System.AppDomain.CurrentDomain.UseClearInsightsExceptionHandling(host.Services.GetRequiredService<ILogger<Program>>());
+
+var myService = host.Services.GetRequiredService<IMyService>();
+await myService.ProcessComplete();
+await myService.ProcessCompleteWithError();
+
+
+
